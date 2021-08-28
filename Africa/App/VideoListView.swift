8aceee391492bdfe67536bc7807f3fcc44c3e2d0
Gustavo Silva <gstvds@icloud.com>
2021-08-28 18:06:23 -0300

    feat: Adds VideoListView

--- Africa/App/VideoListView.swift
+++ Africa/App/VideoListView.swift
@@ -1,5 +1,5 @@
 //
-//  VideoListView.swift.swift
+//  VideoListView.swift
 //  Africa
 //
 //  Created by Gustavo Silva on 27/08/21.
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct VideoListView: View {
+    // MARK: - Properties
+    var videos: [Video] = Bundle.main.decode("videos.json")
+    
+    // MARK: - Body
     var body: some View {
-        Text("Videos")
+        NavigationView {
+            List {
+                ForEach(videos) { video in
+                    VideoListItemView(video: video)
+                        .padding(.vertical, 8)
+                } //: ForEach
+            } //: List
+            .listStyle(InsetGroupedListStyle())
+            .navigationBarTitle("Videos", displayMode: .inline)
+        } //: NavigationView
     }
 }
 
+// MARK: - Preview
 struct VideoListView_Previews: PreviewProvider {
     static var previews: some View {
         VideoListView()
