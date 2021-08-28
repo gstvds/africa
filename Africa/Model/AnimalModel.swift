4a007e6f49c79b891c6d9c5b614b570cbb54a8d9
Gustavo Silva <gstvds@icloud.com>
2021-08-27 22:23:25 -0300

    feat: Adds AnimalModel

---
+++ Africa/Model/AnimalModel.swift
@@ -0,0 +1,19 @@
+//
+//  AnimalsModel.swift
+//  Africa
+//
+//  Created by Gustavo Silva on 27/08/21.
+//
+
+import SwiftUI
+
+struct Animal: Codable, Identifiable {
+    let id: String
+    let name: String
+    let headline: String
+    let description: String
+    let link: String
+    let image: String
+    let gallery: [String]
+    let fact: [String]
+}
