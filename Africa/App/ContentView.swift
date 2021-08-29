99172968b504e64b4fbb3a43fac941654b8a1313
Gustavo Silva <gstvds@icloud.com>
2021-08-29 13:27:47 -0300

    feat: Adds ToolbarItem to ContentView and adds conditional render to ListView

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,23 +9,53 @@ import SwiftUI
 
 struct ContentView: View {
     // MARK: - Properties
+    @State private var isGridViewActive: Bool = false
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
     
     // MARK: - Body
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-
-                ForEach(self.animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    } //: NavigationLink
-                } //: ForEach
-            } //: List
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    
+                        ForEach(self.animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } //: NavigationLink
+                        } //: ForEach
+                    } //: List
+                } else {
+                    Text("")
+                } //: Condition
+            } //: Group
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        Button(action: {
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        } //: Button (List)
+                        Button(action: {
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.grid.2x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        } //: Button (Grid)
+                    } //: HStack
+                } //: ToolbarItem
+            } //: toolbar
         } //: NavigationView
     }
 }
